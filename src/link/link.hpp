2d0b1d3a1f4bfca2ef4825547377c68874dc0ded
Andre Heinemans <andre.heinemans@nxp.com>
2025-04-07 13:34:45 +0200

    Extract interface from udp_link
    Signed-off-by: Andre Heinemans <andre.heinemans@nxp.com>

---
+++ src/link/link.hpp
@@ -0,0 +1,19 @@
+#pragma once
+
+#include <chrono>
+
+class SynapseRos;
+
+class Link {
+
+public:
+    SynapseRos* ros_ { NULL };
+    virtual void run_for(std::chrono::seconds sec) = 0;
+    virtual void write(const uint8_t* buf, uint32_t len) = 0;
+
+protected:
+    virtual void parse_protobuf_message(const uint8_t* buf, uint32_t len);
+
+};
+
+// vi: ts=4 sw=4 et
